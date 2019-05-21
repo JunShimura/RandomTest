26c0c0790a9eac5c981998a9e9b3c541a98e91f7
Jun Shimura <junshimura@gmail.com>
2019-05-21 17:37:49 +0900

    Time span watch

--- Assets/Tests/RandomTestScript.cs
+++ Assets/Tests/RandomTestScript.cs
@@ -4,18 +4,20 @@ using NUnit.Framework;
 using System.Collections;
 
 
-public class RandomTestScript {
+public class RandomTestScript
+{
 
     [Test]
-    public void RandomTestScriptSimplePasses() {
+    public void RandomTestScriptSimplePasses()
+    {
         // Use the Assert class to test conditions.
         // https://docs.unity3d.com/ja/current/ScriptReference/Random.Range.html
 
-        int resultInt=0;
-        int resultFloat=0;
+        int resultInt = 0;
+        int resultFloat = 0;
         float floatR = 0;
         int intR = 0;
-
+        float time = Time.realtimeSinceStartup;
         for (int i = 0; i < 1000000000; i++)
         {
             floatR = Random.Range(0.0f, 100.0f);
@@ -23,13 +25,19 @@ public class RandomTestScript {
             {
                 resultFloat++;
             }
-            intR= Random.Range(0, 10);
+        }
+        Debug.Log( "処理時間:"+(Time.realtimeSinceStartup - time));
+        Debug.Log(resultFloat);
+        time = Time.realtimeSinceStartup;
+        for (int i = 0; i < 1000000000; i++)
+        {
+            intR = Random.Range(0, 10);
             if (intR < 2)
             {
                 resultInt++;
             }
         }
-        Debug.Log(resultFloat);
+        Debug.Log("処理時間:" + (Time.realtimeSinceStartup - time));
         Debug.Log(resultInt);
     }
 
